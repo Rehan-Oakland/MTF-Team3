4045c3d17df81b2110dae5f2df907daa01b46908
Rehan Hussain <rehan.hussain@theoaklandgroup.co.uk>
2024-04-29 16:31:37 +0100

    update to use example of full e2e solution

--- api/app.py
+++ api/app.py
@@ -1,16 +1,23 @@
 from flask import Flask, jsonify, request 
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
-
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 cors = CORS(app)
 # TODO: update postgres
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
 
+##EXAMPLE MODEL, needs to moved 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
-
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/', methods = ['GET', 'POST']) 
 def home(): 
@@ -18,7 +25,12 @@ def home():
   
         data = "hello world"
         return jsonify({'data': data}) 
-    
+
+@app.route('/users')
+def get_users():
+    users = Users.query.all()
+    return jsonify([{'id': user.id, 'username': user.username, 'email': user.email} for user in users])
+
 if __name__ == '__main__': 
   
     app.run(debug = True) 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState , useEffect } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, Box, Text, Center } from '@chakra-ui/react';
+
 
 function HomePage() {
+  const [users,setUsers ] = useState([])
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/users');
+      if (!response.ok) {
+        throw new Error('Failed to fetch users');
+      }
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+
   return (
-    <div>
-      <h2>Welcome to Home Page</h2>
-      {/* Add your home page content here */}
-    </div>
+    <Box p={4}>
+      <Center>
+        <Text fontSize="xl" fontWeight="bold">User List</Text>
+      </Center>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Username</Th>
+            <Th>Email</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {users.map(user => (
+            <Tr key={user.id}>
+              <Td>{user.id}</Td>
+              <Td>{user.username}</Td>
+              <Td>{user.email}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   );
 }
+
 
 export default HomePage;
