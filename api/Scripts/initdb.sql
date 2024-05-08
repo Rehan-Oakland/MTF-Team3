d70cb9861f19654ac8d652308b5d8abcbc6b6935
Rehan Hussain <rehan.hussain@theoaklandgroup.co.uk>
2024-05-08 11:23:51 +0100

    updated init db SQL file

--- api/Scripts/initdb.sql
+++ api/Scripts/initdb.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE receipts;
-
 -- User Table
 CREATE TABLE "User" (
     id SERIAL PRIMARY KEY,
@@ -21,47 +19,53 @@ CREATE TABLE Receipt (
     receipt_date DATE,
     receipt_url VARCHAR(255),
     status VARCHAR(20) CHECK (status IN ('Accepted', 'Rejected', 'Pending')) DEFAULT 'Pending',
-    reason TEXT,
+    reason TEXT DEFAULT NULL,
     FOREIGN KEY (user_id) REFERENCES "User"(id)
-);
-
--- Item Table 
-CREATE TABLE Item (
-    id SERIAL PRIMARY KEY,
-    item_name VARCHAR(100),
-    unit VARCHAR (5),
-    unit_price_BDT INT, 
-);
-
--- School Table 
-CREATE TABLE School (
-    id SERIAL PRIMARY KEY,
-    school_name VARCHAR(100),
-    country VARCHAR(50),
-    address TEXT,
-    start_date DATE,
-);
-
--- Project Table
-CREATE TABLE Project (
-    id SERIAL PRIMARY KEY,
-    project_code VARCHAR (100),
-    project_name VARCHAR (100),
-    country VARCHAR (50),
-    start_date DATE,
 );
 
 -- Purchase Table 
 CREATE TABLE Purchase (
     id SERIAL PRIMARY KEY,
-    FOREIGN KEY (user_id) REFERENCES "User"(id),
-    FOREIGN KEY (item_id) REFERENCES Item (id),
-    FOREIGN KEY (project_code) REFERENCES Project (id),
-    FOREIGN KEY (school_name) REFERENCES School (id),
+    user_id INT,
+    item VARCHAR(50),
+    project_code VARCHAR(50),
+    school_name VARCHAR(100),
+    receipt_id INT,
     quantity INT, 
     total_cost_BDT INT, 
-    date_purchased,
+    date_purchased DATE
+    FOREIGN KEY (user_id) REFERENCES "User"(id)
+    FOREIGN KEY (receipt_id) REFERENCES "Receipt"(id)
 );
+
+
+-- Item Table 
+-- CREATE TABLE Item (
+--     id SERIAL PRIMARY KEY,
+--     item_name VARCHAR(100),
+--     unit VARCHAR (5),
+--     unit_price_BDT INT
+-- );
+
+-- School Table 
+-- CREATE TABLE School (
+--     id SERIAL PRIMARY KEY,
+--     school_name VARCHAR(100),
+--     country VARCHAR(50),
+--     address TEXT,
+--     start_date DATE
+-- );
+
+-- Project Table
+-- CREATE TABLE Project (
+--     id SERIAL PRIMARY KEY,
+--     project_code VARCHAR (100),
+--     project_name VARCHAR (100),
+--     country VARCHAR (50),
+--     start_date DATE
+-- );
+
+
 
 
 
